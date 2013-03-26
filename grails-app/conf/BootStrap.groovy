5216c7f7a213143b2ffe8759365dd9e4a893ad05
Javier Pelado <javier.pelado@gmail.com>
2013-03-26 22:19:42 +0100

    cambio

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -3,19 +3,7 @@ import grails.util.GrailsUtil
 
 class BootStrap { 
 	def init = { servletContext -> 
-		def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true)
-		def userRole = new Role(authority: 'ROLE_USER').save(flush: true)
-
-		def testUser = new User(username: 'admin', enabled: true, password: 'password')
-		testUser.save(flush: true)
-
-		UserRole.create testUser, adminRole, true
-
-		assert User.count() == 1
-		assert Role.count() == 2
-		assert UserRole.count() == 1
-
-/*		switch(GrailsUtil.environment){
+		switch(GrailsUtil.environment){
 	      case "development":
 			// Check whether the test data already exists. 
 			def sectores = ["Almagro",
@@ -65,21 +53,7 @@ class BootStrap {
 			assert Role.count() == 2
 			assert UserRole.count() == 1
 //		}
-
-	      case "development":
-			def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true)
-			def userRole = new Role(authority: 'ROLE_USER').save(flush: true)
-
-			def testUser = new User(username: 'admin', enabled: true, password: 'password')
-			testUser.save(flush: true)
-
-			UserRole.create testUser, adminRole, true
-
-			assert User.count() == 1
-			assert Role.count() == 2
-			assert UserRole.count() == 1
- 
-		}*/
+		} 
 	}
 
 	def destroy = { } 
