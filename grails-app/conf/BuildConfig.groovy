fef4db5c15b2958f4271d6649d6139dd89cb2577
Javier Pelado <javier.pelado@gmail.com>
2013-03-26 08:31:58 +0100

    funcionando la autenticacion

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -59,5 +59,9 @@ grails.project.dependency.resolution = {
         runtime ":database-migration:1.2.1"
 
         compile ':cache:1.0.1'
+
+        compile ':heroku:1.0.1'
+        compile ':cloud-support:1.0.8'
+
     }
 }
