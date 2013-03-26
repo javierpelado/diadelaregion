f5f98d3716cd9ff53e392b34324eff8d57bf8c48
Javier Pelado <javier.pelado@gmail.com>
2013-03-26 22:14:46 +0100

    cambio

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -3,7 +3,19 @@ import grails.util.GrailsUtil
 
 class BootStrap { 
 	def init = { servletContext -> 
-		switch(GrailsUtil.environment){
+		def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true)
+		def userRole = new Role(authority: 'ROLE_USER').save(flush: true)
+
+		def testUser = new User(username: 'admin', enabled: true, password: 'password')
+		testUser.save(flush: true)
+
+		UserRole.create testUser, adminRole, true
+
+		assert User.count() == 1
+		assert Role.count() == 2
+		assert UserRole.count() == 1
+
+/*		switch(GrailsUtil.environment){
 	      case "development":
 			// Check whether the test data already exists. 
 			def sectores = ["Almagro",
@@ -67,7 +79,7 @@ class BootStrap {
 			assert Role.count() == 2
 			assert UserRole.count() == 1
  
-		}
+		}*/
 	}
 
 	def destroy = { } 
