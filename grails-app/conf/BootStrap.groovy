34c3d8f1a400e3279c5adf338765fe17c415765e
Javier Pelado <javier.pelado@gmail.com>
2013-03-26 22:08:26 +0100

    cambio

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -53,7 +53,21 @@ class BootStrap {
 			assert Role.count() == 2
 			assert UserRole.count() == 1
 //		}
-		} 
+		}
+
+	      case "development":
+			def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true)
+			def userRole = new Role(authority: 'ROLE_USER').save(flush: true)
+
+			def testUser = new User(username: 'admin', enabled: true, password: 'password')
+			testUser.save(flush: true)
+
+			UserRole.create testUser, adminRole, true
+
+			assert User.count() == 1
+			assert Role.count() == 2
+			assert UserRole.count() == 1
+ 
 	}
 
 	def destroy = { } 
